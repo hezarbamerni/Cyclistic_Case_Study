7a72c41c2163a5a0985d67c3f5df3d4a9a95d7c7
hezarbamerni <116413376+hezarbamerni@users.noreply.github.com>
2023-03-11 02:38:03 -0700

    Create alltrips21_schema.sql

---
+++ alltrips21_schema.sql
@@ -0,0 +1,133 @@
+--I have created a MySQL table called "Alltrips21" in my "cyclistic_study" database, which includes 13 columns that define the different attributes of each bike ride taken in 2021 by Cyclistic's customers. After importing data from tables named jan21 through dec21, I wanted to create a view that makes it easier to analyze this data, so I created a new view called "alltrips21v".
+
+--This view includes all the columns from the original table, as well as three additional columns that calculate the length of each ride, extract the day of the week and month name from the start time using the DATE_FORMAT function. I also included several conditions in the WHERE clause to filter out any rows with missing or invalid data.
+
+--Creating this view has allowed me to analyze the data in a more convenient format, and I can now use SQL queries that refer to this view instead of the original table to gain insights into Cyclistic's bike-share service usage throughout 2021.
+
+-- CREATE A TABLE ALLTRIPS21
+CREATE TABLE cyclistic_study.Alltrips21 (
+  ride_id VARCHAR(500),
+  rideable_type VARCHAR(255),
+  started_at VARCHAR(255),
+  ended_at VARCHAR(255),
+  start_station_name VARCHAR(255),
+  start_station_id VARCHAR(255),
+  end_station_name VARCHAR(255),
+  end_station_id VARCHAR(255),
+  start_lat VARCHAR(255),
+  start_lng VARCHAR(255),
+  end_lat VARCHAR(255),
+  end_lng VARCHAR(255),
+  member_casual VARCHAR(255)
+);
+
+--INSERT ALL TABLES FROM JAN21 THROUGH DEC21 INTO ONE TABLE, ALLTRIPS21
+INSERT INTO alltrips21 (id, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual)
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM jan21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM feb21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM mar21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM apr21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM may21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM jun21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM jul21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM aug21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM sep21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM oct21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM nov21
+UNION ALL
+SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
+FROM dec21
+ORDER BY started_at;
+
+
+--Calculates the total number of bike trips, the number of unique trips, and the average trip duration (excluding rows with missing or invalid data, and trips with duration less than 59 seconds).
+SELECT 
+    COUNT(*) AS total_trips,
+    AVG(TIMESTAMPDIFF(SECOND, started_at, ended_at)) AS average_duration
+FROM 
+    alltrips21
+WHERE 
+	ride_id IS NOT NULL
+	AND CHAR_LENGTH(ride_id) = 16
+    AND started_at < ended_at
+    AND started_at IS NOT NULL
+    AND ended_at IS NOT NULL
+    AND start_station_name IS NOT NULL
+    AND start_station_id IS NOT NULL
+    AND end_station_name IS NOT NULL
+    AND end_station_id IS NOT NULL
+    AND start_lat IS NOT NULL
+    AND start_lng IS NOT NULL
+    AND end_lat IS NOT NULL
+    AND end_lng IS NOT NULL
+    AND member_casual IS NOT NULL
+    AND TIMESTAMPDIFF(SECOND, started_at, ended_at) >= 59;
+	
+--Create a view to run queries. clean and transform the data: The data may need to be cleaned and transformed before it can be analyzed. 
+CREATE OR REPLACE VIEW alltrips21v AS
+SELECT 
+  id,
+  ride_id, 
+  rideable_type, 
+  started_at, 
+  ended_at, 
+  start_station_name, 
+  start_station_id, 
+  end_station_name, 
+  end_station_id, 
+  start_lat, 
+  start_lng, 
+  end_lat, 
+  end_lng, 
+  member_casual, 
+  TIMESTAMPDIFF(SECOND, STR_TO_DATE(started_at, '%Y-%m-%d %H:%i:%s'), STR_TO_DATE(ended_at, '%Y-%m-%d %H:%i:%s')) AS ride_length,
+  DATE_FORMAT(STR_TO_DATE(started_at, '%Y-%m-%d %H:%i:%s'), '%W') AS day_of_week,
+  DATE_FORMAT(STR_TO_DATE(started_at, '%Y-%m-%d %H:%i:%s'), '%M') AS month_name 
+FROM alltrips21
+WHERE id IS NOT NULL
+  AND ride_id IS NOT NULL 
+  AND CHAR_LENGTH(ride_id) = 16 
+  AND started_at < ended_at
+  AND started_at IS NOT NULL 
+  AND ended_at IS NOT NULL 
+  AND start_station_name IS NOT NULL 
+  AND start_station_id IS NOT NULL 
+  AND end_station_name IS NOT NULL 
+  AND end_station_id IS NOT NULL 
+  AND start_lat IS NOT NULL 
+  AND start_lng IS NOT NULL 
+  AND end_lat IS NOT NULL 
+  AND end_lng IS NOT NULL
+  AND member_casual IS NOT NULL
+  AND TIMESTAMPDIFF(SECOND, started_at, ended_at) >= 60;
+  
+--row count for alltrips21 table and all views
+SELECT
+  (SELECT COUNT(*) FROM alltrips21) AS Total_rides,
+  (SELECT COUNT(*) FROM alltrips21v) AS Cleaned_data,
+  (SELECT COUNT(*) FROM alltrips_casual) AS Casual_riders_count,
+  (SELECT COUNT(*) FROM alltrips_member) AS Member_riders_count,
+  (SELECT COUNT(*) FROM alltrips_rtcb) AS Classic_bike_count,
+  (SELECT COUNT(*) FROM alltrips_rtdb) AS Docked_bike_count,
+  (SELECT COUNT(*) FROM alltrips_rteb) AS Electric_bike_count;
