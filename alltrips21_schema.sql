4b203d45ecd618d92cf622f75186024de35e7a2d
hezarbamerni <116413376+hezarbamerni@users.noreply.github.com>
2023-03-13 15:18:48 -0700

    Update alltrips21_schema.sql

--- alltrips21_schema.sql
+++ alltrips21_schema.sql
@@ -1,14 +1,15 @@
 /*
-+I have created a MySQL table called "Alltrips21" in my "cyclistic_study" database, which includes 13 columns that define the different
-+attributes of each bike ride taken in 2021 by Cyclistic's customers. After importing data from tables named jan21 through dec21, I wanted
-+to create a view that makes it easier to analyze this data, so I created a new view called "alltrips21v". This view includes all the columns
-+from the original table, as well as three additional columns that calculate the length of each ride, extract the day of the week and month name
-+from the start time using the DATE_FORMAT function. I also included several conditions in the WHERE clause to filter out any rows with missing
-+or invalid data. Creating this view has allowed me to analyze the data in a more convenient format, and I can now use SQL queries that refer to
-+this view instead of the original table to gain insights into Cyclistic's bike-share service usage throughout 2021.
++I have created a MySQL table called "Alltrips21" which includes 13 columns that define the different attributes of each bike ride taken in 2021
++by Cyclistic's customers. After importing data from tables named jan21 through dec21, I wanted to create a view that makes it easier to analyze
++this data, so I created a new view called "alltrips21v". This view includes all the columns from the original table, as well as three additional
++columns that calculate the length of each ride, extract the day of the week and month name from the start time using the DATE_FORMAT function.
++I also included several conditions in the WHERE clause to filter out any rows with missing or invalid data. Creating this view has allowed me
++to analyze the data in a more convenient format, and I can now use SQL queries that refer to this view instead of the original table to gain
++insights into Cyclistic's bike-share service usage throughout 2021.
++Tables jan21 through dec21 were created prior to this step.
 */
 
--- CREATE A TABLE ALLTRIPS21
+-- Create a table called alltrips21
 CREATE TABLE cyclistic_study.Alltrips21 (
   ride_id VARCHAR(500),
   rideable_type VARCHAR(255),
@@ -25,7 +26,7 @@ CREATE TABLE cyclistic_study.Alltrips21 (
   member_casual VARCHAR(255)
 );
 
---INSERT ALL TABLES FROM JAN21 THROUGH DEC21 INTO ONE TABLE, ALLTRIPS21
+-- Insert all tables from JAN21 through DEC21 into alltrips21 table
 INSERT INTO alltrips21 (id, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual)
 SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
 FROM jan21
@@ -64,31 +65,8 @@ SELECT null, ride_id, rideable_type, started_at, ended_at, start_station_name, s
 FROM dec21
 ORDER BY started_at;
 
-
---Calculates the total number of bike trips, the number of unique trips, and the average trip duration (excluding rows with missing or invalid data, and trips with duration less than 59 seconds).
-SELECT 
-    COUNT(*) AS total_trips,
-    AVG(TIMESTAMPDIFF(SECOND, started_at, ended_at)) AS average_duration
-FROM 
-    alltrips21
-WHERE 
-	ride_id IS NOT NULL
-	AND CHAR_LENGTH(ride_id) = 16
-    AND started_at < ended_at
-    AND started_at IS NOT NULL
-    AND ended_at IS NOT NULL
-    AND start_station_name IS NOT NULL
-    AND start_station_id IS NOT NULL
-    AND end_station_name IS NOT NULL
-    AND end_station_id IS NOT NULL
-    AND start_lat IS NOT NULL
-    AND start_lng IS NOT NULL
-    AND end_lat IS NOT NULL
-    AND end_lng IS NOT NULL
-    AND member_casual IS NOT NULL
-    AND TIMESTAMPDIFF(SECOND, started_at, ended_at) >= 59;
 	
---Create a view of the clean data to run queries for analysis. 
+-- Create a view of the clean data to run queries for analysis. 
 CREATE OR REPLACE VIEW alltrips21v AS
 SELECT 
   id,
