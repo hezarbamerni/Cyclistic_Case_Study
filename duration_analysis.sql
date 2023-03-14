984236f924900aba092030df07f7b1098855d937
hezarbamerni <116413376+hezarbamerni@users.noreply.github.com>
2023-03-13 21:25:19 -0700

    Update duration_analysis.sql

--- duration_analysis.sql
+++ duration_analysis.sql
@@ -1,12 +1,9 @@
 
 /*
-+The following queries are based on the view table called 'alltrips21v', which is a clean data table containing processed and formatted data from the original source.
-+
-+
-+This query finds the maximum, minimum, and average ride duration for each combination of rider type (member_casual)
-+and day of the week (day_of_week). It does this by first selecting the member_casual, day_of_week, and ride_length
-+columns from the alltrips21v view, and then grouping the results by member_casual and day_of_week.
-+The maximum, minimum, and average ride durations are calculated using the MAX(), MIN(), and AVG() aggregate functions, respectively.
++This query finds the maximum, minimum, and average ride duration for each combination of rider type (member_casual) and day of the week (day_of_week).
++It does this by first selecting the member_casual, day_of_week, and ride_length columns from the alltrips21v view, and then grouping the results by 
++member_casual and day_of_week. The maximum, minimum, and average ride durations are calculated using the MAX(), MIN(), and AVG() aggregate functions,
++respectively.
 */
 
 SELECT 
@@ -48,9 +45,8 @@ GROUP BY alias.member_casual,
 
 
 /*
-+This query finds the maximum, minimum, and average ride duration for each rider type (member_casual).
-+It does this by selecting the member_casual and ride_length columns from the alltrips21v view, and
-+then grouping the results by member_casual. The maximum, minimum, and average ride durations are again
++This query finds the maximum, minimum, and average ride duration for each rider type (member_casual). It does this by selecting the member_casual and
++ride_length columns from the alltrips21v view, and then grouping the results by member_casual. The maximum, minimum, and average ride durations are again
 +calculated using the MAX(), MIN(), and AVG() aggregate functions.
 */
 
@@ -74,9 +70,8 @@ GROUP BY   alias.member_casual;
 
 
 /*
-+This query finds the maximum, minimum, and average ride duration for each day of the week (day_of_week).
-+It does this by selecting the day_of_week and ride_length columns from the alltrips21v view, and
-+then grouping the results by day_of_week. The maximum, minimum, and average ride durations are again
++This query finds the maximum, minimum, and average ride duration for each day of the week (day_of_week). +It does this by selecting the day_of_week and
++ride_length columns from the alltrips21v view, and then grouping the results by day_of_week. The maximum, minimum, and average ride durations are again
 +calculated using the MAX(), MIN(), and AVG() aggregate functions.
 */
 
